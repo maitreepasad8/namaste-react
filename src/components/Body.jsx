13d551b4cb6c7bc0691bc6372be1577104f0bc5e
Maitree <maitree.pasad@udaan.com>
2023-10-08 19:54:32 +0530

    episode 6

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,24 +1,73 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../utils/mockData";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [restList, setRestList] = useState(restaurants);
+  const [restList, setRestList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRest, setFilteredRest] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setRestList(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map(
+        (r) => r.info
+      )
+    );
+    setFilteredRest(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map(
+        (r) => r.info
+      )
+    );
+  };
+
+  if (restList.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setFilteredRest(
+                restList.filter((r) =>
+                  r.name.toLowerCase().includes(searchText)
+                )
+              );
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            setRestList(restList.filter((r) => r.avgRating > 4.2));
+            setFilteredRest(restList.filter((r) => r.avgRating > 4.2));
           }}
         >
           Filter
         </button>
       </div>
       <div className="res-container">
-        {restList.map((r) => (
+        {filteredRest.map((r) => (
           <RestaurantCard restaurant={r} key={r.id} />
         ))}
       </div>
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -10,8 +10,8 @@ const RestaurantCard = ({ restaurant }) => {
         src={CDN_URL + cloudinaryImageId}
       />
       <h3>{name}</h3>
-      <h4>{cuisines.toString()}</h4>
-      <h4>{avgRating}</h4>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} ⭐️</h4>
     </div>
   );
 };
